d4ca79ec75c8814f36f462f0e0c43d62743da37c
Kelio <keliojosue2@gmail.com>
2023-08-24 00:32:34 -0300

    Updates

--- client/start_http-server.py
+++ client/start_http-server.py
@@ -8,5 +8,6 @@ Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving at port {PORT}")
+    print("You can close this window once you're done using the app\n")
     webbrowser.open(f"http://localhost:{PORT}")
     httpd.serve_forever()
